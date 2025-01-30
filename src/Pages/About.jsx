cf977095e3ec925b82ef6d450f350284133231a9
madoelmalah4 <madoelmalah4@gmail.com>
2025-01-30 18:39:53 +0200

    frsfr

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -5,7 +5,6 @@ import { Reveal } from "../Components/Reveal";
 import aboutImg3 from "../assets/aboutImg3.jpg";
 import { RevealText } from "../Components/RevealText";
 import { RevealLeftARight } from "../Components/RevealLeftARight";
-import Sewedy from '../assets/Sewedy.png'
 const About = () => {
   return (
     <Stack
@@ -17,35 +16,7 @@ const About = () => {
         overflow: "hidden", // Prevent overflow
       }}
     >
-      <Stack
-        sx={{
-          width: "100vw",
-          minHeight: "100vh",
-          justifyContent: "flex-start",
-          alignItems: "center",
-          overflowX: "hidden",
-          position: "relative",
-        }}
-      >
-        <Box
-          sx={{
-            position: "absolute",
-            top: 0,
-            left: 0,
-            width: "100%",
-            height: "100%",
-            backgroundImage: `url(${Sewedy})`,
-            backgroundSize: "cover",
-            backgroundPosition: "center",
-            backgroundRepeat: "no-repeat",
-            filter: "blur(0.7px)",
-            maskImage: "linear-gradient(to top, transparent, black)",
-            WebkitMaskImage: "linear-gradient(to top, transparent, black)",
-            zIndex: -1,
-          }}
-        />
-       
-      </Stack>
+     
     </Stack>
   );
 };
